532f430104eddbec7ad955d668e45295c75bb04c
Kevin Lin <33496321+Stuy-Kevin-Lin@users.noreply.github.com>
2017-12-12 17:54:09 -0500

    HW52

--- HW52/InsertionSort.java
+++ HW52/InsertionSort.java
@@ -1,6 +1,11 @@
+//Kevin Lin
+//APCS1 pd8
+//HW52 -- Poker Face
+//2017-12-13
+
 /***************************************
- *  class InsertionSort -- implements InsertionSort algorithm
- ***************************************/
+*  class InsertionSort -- implements InsertionSort algorithm
+***************************************/
 
 import java.util.ArrayList;
 
@@ -13,9 +18,9 @@ public class InsertionSort
   public static ArrayList populate( int size, int lo, int hi ) {
     ArrayList<Integer> retAL = new ArrayList<Integer>();
     while( size > 0 ) {
-	    //     offset + rand int on interval [lo,hi]
-	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
-	    size--;
+      //     offset + rand int on interval [lo,hi]
+      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+      size--;
     }
     return retAL;
   }
@@ -24,9 +29,9 @@ public class InsertionSort
   public static void shuffle( ArrayList al ) {
     int randomIndex;
     for( int i = al.size()-1; i > 0; i-- ) {
-	    //pick an index at random
+      //pick an index at random
       randomIndex = (int)( (i+1) * Math.random() );
-	    //swap the values at position i and randomIndex
+      //swap the values at position i and randomIndex
       al.set( i, al.set( randomIndex, al.get(i) ) );
     }
   }
@@ -38,12 +43,13 @@ public class InsertionSort
   // postcondition: data's elements sorted in ascending order
   public static void insertionSortV( ArrayList<Comparable> data )
   {
-      for (int pass = 0; pass < data.size()-1; pass++) {
-	  int currentIndex = pass + 1; //To track the number
-	  while (data.get(currentIndex).compareTo(data.get(currentIndex - 1)) < 0) {
-	      data.set(currentIndex,data.set(currentIndex-1, data.get(currentIndex)));
-	  }
+    for (int pass = 0; pass < data.size()-1; pass++) {
+      int currentIndex = pass + 1; //To track the number
+      while (currentIndex != 0 && data.get(currentIndex).compareTo(data.get(currentIndex - 1)) < 0) {
+        data.set(currentIndex,data.set(currentIndex-1, data.get(currentIndex)));
+        currentIndex--;
       }
+    }
   }//end insertionSortV
 
 
@@ -51,54 +57,59 @@ public class InsertionSort
   // postcondition: order of input ArrayList's elements unchanged
   //                Returns sorted copy of input ArrayList.
   public static ArrayList<Comparable>
-    insertionSort( ArrayList<Comparable> input )
+  insertionSort( ArrayList<Comparable> input )
   {
-    /* YOUR IMPLEMENTATION HERE */
+    ArrayList<Comparable> out = new ArrayList();
+    for (int i = 0; i < input.size(); i++) {
+      out.add(input.get(i)); //Copies list
+    }
+    insertionSortV(out);
+    return out;
   }//end insertionSort
 
 
   public static void main( String [] args )
   {
     /*===============for VOID methods=============
-      System.out.println("\n*** Testing sort-in-place (void) version... *** ");
-      ArrayList glen = new ArrayList<Integer>();
-      glen.add(7);
-      glen.add(1);
-      glen.add(5);
-      glen.add(12);
-      glen.add(3);
-      System.out.println( "\nArrayList glen before sorting:\n" + glen );
-      insertionSortV(glen);
-      System.out.println( "\nArrayList glen after sorting:\n" + glen );
+    System.out.println("\n*** Testing sort-in-place (void) version... *** ");
+    ArrayList glen = new ArrayList<Integer>();
+    glen.add(7);
+    glen.add(1);
+    glen.add(5);
+    glen.add(12);
+    glen.add(3);
+    System.out.println( "\nArrayList glen before sorting:\n" + glen );
+    insertionSortV(glen);
+    System.out.println( "\nArrayList glen after sorting:\n" + glen );
 
-      ArrayList coco = populate( 10, 1, 1000 );
-      System.out.println( "\nArrayList coco before sorting:\n" + coco );
-      insertionSortV(coco);
-      System.out.println( "\nArrayList coco after sorting:\n" + coco );
-      ============================================*/
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "\nArrayList coco before sorting:\n" + coco );
+    insertionSortV(coco);
+    System.out.println( "\nArrayList coco after sorting:\n" + coco );
+    ============================================*/
 
+
+    System.out.println( "*** Testing non-void version... *** " );
+    ArrayList glen = new ArrayList<Integer>();
+    glen.add(7);
+    glen.add(1);
+    glen.add(5);
+    glen.add(12);
+    glen.add(3);
+    System.out.println( "\nArrayList glen before sorting:\n" + glen );
+    ArrayList glenSorted = insertionSort( glen );
+    System.out.println( "\nsorted version of ArrayList glen:\n"
+    + glenSorted );
+    System.out.println( "\nArrayList glen after sorting:\n" + glen );
+
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "\nArrayList coco before sorting:\n" + coco );
+    ArrayList cocoSorted = insertionSort( coco );
+    System.out.println( "\nsorted version of ArrayList coco:\n"
+    + cocoSorted );
+    System.out.println( "\nArrayList coco after sorting:\n" + coco );
     /*==========for AL-returning methods==========
-      System.out.println( "*** Testing non-void version... *** " );
-    	ArrayList glen = new ArrayList<Integer>();
-      glen.add(7);
-      glen.add(1);
-      glen.add(5);
-      glen.add(12);
-      glen.add(3);
-      System.out.println( "\nArrayList glen before sorting:\n" + glen );
-      ArrayList glenSorted = insertionSort( glen );
-      System.out.println( "\nsorted version of ArrayList glen:\n" 
-      + glenSorted );
-      System.out.println( "\nArrayList glen after sorting:\n" + glen );
-
-      ArrayList coco = populate( 10, 1, 1000 );
-      System.out.println( "\nArrayList coco before sorting:\n" + coco );
-      ArrayList cocoSorted = insertionSort( coco );
-      System.out.println( "\nsorted version of ArrayList coco:\n" 
-      + cocoSorted );
-      System.out.println( "\nArrayList coco after sorting:\n" + coco );
-      System.out.println( coco );
-      ============================================*/
+    ============================================*/
 
   }//end main
 
